9fa6da1edde1c3c92ad4185d47398123c84d18c7
Samet ALTUNDAG <smtaltundag63@gmail.com>
2023-12-20 19:08:02 +0300

    Layouts işlemleri tamamlandı.

---
+++ ders2/resources/views/about.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+@section('title') Anasayfa @endsection
+@section('css')
+    <style>
+        body {
+            background-color: #919090;
+        }
+    </style>
+@endsection
+
+@section('content')
+<div class="row">
+    <h1>samet altundağ</h1>
+    <p>merhaba</p>
+</div>
+@endsection
+
+@section('js')
+    <script src="#"></script>
+@endsection
