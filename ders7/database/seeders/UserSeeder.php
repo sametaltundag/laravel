8681ad2e951412e564c707df9196c941298cc3bd
Samet ALTUNDAG <smtaltundag63@gmail.com>
2023-12-23 14:11:14 +0300

    Seeder ile birden fazla veri ekleme tamamlandı.

--- ders7/database/seeders/UserSeeder.php
+++ ders7/database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
@@ -13,6 +14,8 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        /* // Seeder ile tekil veri ekleme
+
         DB::table('users')->truncate();
         DB::table('users')->insert([
             'name' => 'John Doe',
@@ -21,6 +24,38 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
             'remember_token' => '1234567890',
             'current_team_id' => 1,
-        ]);   
+        ]);   */
+
+        // Seeder ile birden fazla veri ekleme
+        User::query()->truncate();
+        $data = [
+            [
+                'name' => 'John Doe',
+                'email' => 'fVJ5u@example.com',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now()
+            ],
+            [
+                'name' => 'Jane Doe',
+                'email' => 'qY5Oy@example.com',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now()
+            ],
+            [
+                'name' => 'Mark Doe',
+                'email' => 'XUqC8@example.com',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now()
+            ],
+            [
+                'name' => 'Mary Doe',
+                'email' => 'qGx0A@example.com',
+                'password' => bcrypt('password'),
+                'email_verified_at' => now()
+            ]
+            ];
+
+            User::query()->insert([$data]);
+
     }
 }
